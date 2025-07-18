26ff9a3722ac1d8611ea442c6e73fd4193e55cae
Viktoriia Popel <xructa2009@gmail.com>
2025-07-18 15:16:30 +0200

    fix chanches

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('name-input');
-const output = document.getElementById('name-output');
-
-input.addEventListener('input', event => {
-  const value = event.currentTarget.value.trim();
-  output.textContent = value === '' ? 'Anonymous' : value;
-});
